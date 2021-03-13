87c9f1aa31b63a1f82a627b67b14dd0b4a3fd180
santiweide <ggy_778@sina.com>
2021-03-13 09:35:13 +0800

    fix

--- src/view/gui/draw/EllipseShape.java
+++ src/view/gui/draw/EllipseShape.java
@@ -11,7 +11,7 @@ import java.awt.*;
 
 public class EllipseShape implements IShape {
     /**
-     * position info
+     * position inf:wq:o
      */
     private final PositionConfig positionConfig;
     /**
